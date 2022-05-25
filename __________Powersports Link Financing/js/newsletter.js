a48082ca188670a824729bab7c56f3f6db057884
Taha Mostafa <tahamostfa588@gmail.com>
2022-05-25 15:03:16 +0200

    fix header & add footer pages & other things

---
+++ __________Powersports Link Financing/js/newsletter.js
@@ -0,0 +1,11 @@
+!(function (c, h, i, m, p) {
+  (m = c.createElement(h)),
+    (p = c.getElementsByTagName(h)[0]),
+    (m.async = 1),
+    (m.src = i),
+    p.parentNode.insertBefore(m, p);
+})(
+  document,
+  'script',
+  'https://chimpstatic.com/mcjs-connected/js/users/48914c65fc2d2c8eabc541e36/2f6d9d5f2ad54b2­976e180d52.js'
+);
--- ________________Turple Bros Ltd/js/main.js
+++ ________________Turple Bros Ltd/js/main.js
@@ -39,3 +39,48 @@ $(function () {
 /* End Handle Fake anchors */
 
 // Website Scripts
+
+// Handle header logo
+var oldContent = $('.nav.navbar-nav').find('>li:nth-child(1) a').html();
+$(document).ready(function () {
+  $('.nav.navbar-nav')
+    .find('>li:nth-child(1) a')
+    .html(
+      '<img src="/site-uploads/1001398/images/logo.svg" style="margin: 1px;max-width: 80%;">'
+    );
+
+  function Change_site_menu_content() {
+    if (window.innerWidth > 767) {
+      $('.nav.navbar-nav')
+        .find('>li:nth-child(1) a')
+        .html(
+          '<img src="/site-uploads/1001398/images/logo.svg" style="margin: 1px;max-width: 95%;">'
+        );
+    } else {
+      $('.nav.navbar-nav').find('>li:nth-child(1) a').html(oldContent);
+      $('.nav.navbar-nav')
+        .find('>li:nth-child(1) a')
+        .css({ 'font-size': '16px' });
+    }
+  }
+  $(function () {
+    Change_site_menu_content();
+  });
+
+  $(window).on('resize', function () {
+    Change_site_menu_content();
+  });
+  /* End Change site menu content for mobile */
+});
+
+$('.letters-list a').click(function (e) {
+  e.preventDefault();
+  var divId = $(this).attr('href');
+  console.log($(this).attr('href'));
+  $('html, body').animate(
+    {
+      scrollTop: $(divId).offset().top - 40, //#DIV_ID is an example. Use the id of your destination on the page
+    },
+    'slow'
+  );
+});
