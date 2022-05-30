fe59dac28a8a1e662113456b3671b16b253a20ac
Taha Mostafa <tahamostfa588@gmail.com>
2022-05-31 01:47:22 +0200

    add some work for Turple Bros Ltd & Powersports Link Financing & Lockharts

--- __________Powersports Link Financing/js/newsletter.js
+++ __________Powersports Link Financing/js/newsletter.js
@@ -9,3 +9,21 @@
   'script',
   'https://chimpstatic.com/mcjs-connected/js/users/48914c65fc2d2c8eabc541e36/2f6d9d5f2ad54b2­976e180d52.js'
 );
+
+(function ($) {
+  window.fnames = new Array();
+  window.ftypes = new Array();
+  fnames[0] = 'EMAIL';
+  ftypes[0] = 'email';
+  fnames[1] = 'FNAME';
+  ftypes[1] = 'text';
+  fnames[2] = 'LNAME';
+  ftypes[2] = 'text';
+  fnames[3] = 'ADDRESS';
+  ftypes[3] = 'address';
+  fnames[4] = 'PHONE';
+  ftypes[4] = 'phone';
+  fnames[5] = 'BIRTHDAY';
+  ftypes[5] = 'birthday';
+})(jQuery);
+var $mcj = jQuery.noConflict(true);
